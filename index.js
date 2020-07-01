111e26c97154c92c33676ca949d15958fde91af3
Masonhealy <67674776+Masonhealy@users.noreply.github.com>
2020-07-01 01:38:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'NzI3NzMyMjI4ODQ2NjQ5MzU1.XvwVZw.JZaqncES1Fo9J4txhOEoSmCwuYg';
 
 bot.on('ready', () =>{
     console.log('The BOT is online!');
 })
 
-bot.login(token);
+bot.login(process.env.token);
